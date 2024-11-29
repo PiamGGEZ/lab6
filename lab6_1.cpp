13b1a55a50f8bc182743c43320f0e8129385231b
Piamchok <piumchoke@gmail.com>
2024-11-29 15:16:34 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
-
+ 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int n,Evennumbers = 0,Oddnumbers = 0 ;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> n;
+        if(n==0){
+            break;
+        }
+        else if (n %2==0){
+            Evennumbers++;
+        }
+        else{
+            Oddnumbers++;
+        }
+        
+    }
+    
+    cout << "#Even numbers = "<<Evennumbers;
+    cout << "\n#Odd numbers = "<<Oddnumbers;
     return 0;
 }
